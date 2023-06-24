99646858a65399db739be98e944f69da3b5f458a
Ergvall <Club90msk@gmail.com>
2023-06-24 18:53:56 +0300

    New exemple

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter number : ");
+String strNum = Console.ReadLine();
+int num = Convert.ToInt32(strNum);
+
+int sqr = num  * num;
+
+System.Console.WriteLine($"sqr = {sqr}");
+
+// if(aqr < 0){
+//     sqr*= -1;
+// }
